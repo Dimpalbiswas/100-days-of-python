8aa3869b9df319aebdd2a3f35b26bcb9943d077f
Dimpalbiswas <dimpal.roy879@gmail.com>
2026-07-07 10:38:59 +0530

    Day 01: Printing and Strings

--- Day01/hello.py
+++ Day01/hello.py
@@ -1,2 +1,17 @@
+# "this is a comment"
 print("Hello Dimpal!")
 print("Welcome to Python.")
+print("I am learning python")
+print("100")
+print("cat")
+print("dog")
+print("lion")
+print("tiger")
+print("A\nB\nC")
+
+print("Hello World!")
+print("welcome to python!")
+print("Today is Day 1")
+print("Dimpal\n34\nRewari")
+
+print("*****\n*   *\n*****")
